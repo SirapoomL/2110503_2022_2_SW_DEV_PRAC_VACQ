bddb5976400c23e10fd972c2e412272a1a59618c
Sirapoom Leesuravanich <sirapoomlee@gmail.com>
2023-02-09 01:31:48 +0700

    Connect with MongoDB Atlas

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -1,34 +1,73 @@
+const Hospital = require('../models/Hospitals')
+
 // @desc    Get all hospital
 // @route   GET /api/v1/hospitals
 // @access  Public
-exports.getHospitals=(req,res,next) => {
-    res.status(200).json({success:true, mas:`Get all hospitals`})
+exports.getHospitals=async (req,res,next) => {
+    // res.status(200).json({success:true, mas:`Get all hospitals`})
+    try{
+        const hospitals = await Hospital.find()
+        res.status(200).json({success:true,count:hospitals.length,data:hospitals})
+    } catch(err){
+        res.status(400).json({success:false})
+    }
 }
 
 // @desc    Get single hospital
 // @route   GET /api/v1/hospitals/:id
 // @access  Public
-exports.getHospital=(req,res,next) => {
-    res.status(200).json({success:true, mas:`Get hospital ${req.params.id}`})
+exports.getHospital=async (req,res,next) => {
+    // res.status(200).json({success:true, mas:`Get hospital ${req.params.id}`})
+    try{
+        const hospital = await Hospital.findById(req.params.id)
+        if(!hospital){
+            return res.status(400).json({success:false})
+        }
+        res.status(200).json({success:true,data:hospital})
+    } catch(err){
+        res.status(400).json({success:false})
+    }
 }
 
 // @desc    Create hospital
 // @route   POST /api/v1/hospitals
 // @access  Private
-exports.createHospital=(req,res,next) => {
-    res.status(200).json({success:true, mas:`Create a hospital`})
+exports.createHospital= async (req,res,next) => {
+    const hospital = await Hospital.create(req.body)
+    res.status(201).json({success:true, data:hospital})
 }
 
 // @desc    Update single hospital
 // @route   PUT /api/v1/hospitals/:id
 // @access  Private
-exports.updateHospital=(req,res,next) => {
-    res.status(200).json({success:true, mas:`Update hospital ${req.params.id}`})
+exports.updateHospital= async (req,res,next) => {
+    // res.status(200).json({success:true, mas:`Update hospital ${req.params.id}`})
+    try{
+        const hospital = await Hospital.findByIdAndUpdate(req.params.id,req.body,{
+            new: true,
+            runValidators:true
+        })
+        if(!hospital){
+            return res.status(400).json({success:false})
+        }
+        res.status(200).json({success:true,data:hospital})
+    } catch(err){
+        res.status(400).json({success:false})
+    }
 }
 
 // @desc    Delete all hospital
 // @route   DELETE /api/v1/hospitals
 // @access  Private
-exports.deleteHospital=(req,res,next) => {
-    res.status(200).json({success:true, mas:`Delete hospital ${req.params.id}`})
+exports.deleteHospital= async (req,res,next) => {
+    // res.status(200).json({success:true, mas:`Delete hospital ${req.params.id}`})
+    try{
+        const hospital = await Hospital.findByIdAndDelete(req.params.id)
+        if(!hospital){
+            return res.status(400).json({success:false})
+        }
+        res.status(200).json({success:true,data:{}})
+    } catch(err){
+        res.status(400).json({success:false})
+    }
 }
--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db')
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
-const app = express()
+//Cinnect to database
+connectDB();
 
-// app.get('/', (req,res) => {
-//     // res.send('<h1>Hello from express</h1>')
-//     // res.send({name:'Brad'});
-//     // res.json({name:'Brad'});
-//     // res.sendStatus(400)
-//     // res.status(400).json({success:false})
-//     // res.status(200).json({success:true, data:{id:1}})
-// })
+// Body parser
+const app = express()
+app.use(express.json())
 
 //Mount routers
 const hospitals = require(`./routes/hospitals`)
 app.use(`/api/v1/hospitals` ,hospitals)
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in ',process.env.NODE_ENV, ' mode on port ', PORT))
+const server = app.listen(PORT, console.log('Server running in ',process.env.NODE_ENV, ' mode on port ', PORT))
+
+//Handle unhandle promise rejections
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`)
+    // Close server & exit process
+    server.close((()=>process.exit(1)))
+})
