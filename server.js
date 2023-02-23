2521100febac14db46c2577da1faaf92e07e7dc0
Sirapoom Leesuravanich <sirapoomlee@gmail.com>
2023-02-23 19:35:30 +0700

    assignment 6

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db')
 
 //Load env vars
@@ -12,9 +13,15 @@ connectDB();
 const app = express()
 app.use(express.json())
 
+// Cookie parser
+app.use(cookieParser());
+
 //Mount routers
 const hospitals = require(`./routes/hospitals`)
+const auth = require('./routes/auth')
+
 app.use(`/api/v1/hospitals` ,hospitals)
+app.use('/api/v1/auth',auth)
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running in ',process.env.NODE_ENV, ' mode on port ', PORT))
