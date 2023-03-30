828da73950d013a2360ee29a64ef83a79b054115
Sirapoom Leesuravanich <sirapoomlee@gmail.com>
2023-03-30 18:04:54 +0700

    init frontend

---
+++ vacqfrontend/test/script.js
@@ -0,0 +1,7 @@
+function displaySum() {
+    let firstNum = Number(document.getElementById('firstNum').innerHTML)
+    let secondNum = Number(document.getElementById('secondNum').innerHTML)
+    let total = firstNum + secondNum;
+    document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNumequals} to ${total}` ;
+    }
+document.getElementById('sumButton').addEventListener("click", displaySum)
