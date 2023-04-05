ce38440c56315b2814b34a7119332120e454f33b
Sirapoom Leesuravanich <sirapoomlee@gmail.com>
2023-04-06 01:41:43 +0700

    assignmne10 security and openapi

--- webservice/server.js
+++ webservice/server.js
@@ -10,13 +10,63 @@ dotenv.config({path:'./config/config.env'});
 connectDB();
 
 const cors = require('cors');
-// Body parser
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
+
 const app = express()
-app.use(cors());
+
+// Body parser
 app.use(express.json())
 
 // Cookie parser
 app.use(cookieParser());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate Limiting
+const limiter = rateLimit({
+    windowsMs:10*60*1000,//10 mins
+    max:100
+})
+app.use(limiter)
+
+// Prevent http param pollutions
+app.use(hpp())
+
+// Enable CORS
+app.use(cors());
+
+const swaggerOptions = {
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info: {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A simple Express VacQ API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:5000/api/v1'
+            }
+        ]
+    },
+    apis:['./routes/*.js'],
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 //Mount routers
 const hospitals = require(`./routes/hospitals`)
